f25c7a6e3f1be6c0ef9d591c4f07417b0de007e7
emimamadi <emohamadz2012@gmail.com>
2024-06-10 20:59:57 +0330

    structure website created.

---
+++ redux/store.ts
@@ -0,0 +1,25 @@
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+///------
+
+// import ProductReducer from "@/redux/productSlice";
+
+//----
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { postSlice } from "@/redux/postSlice";
+import productSlice from "@/redux/productSlice";
+import cartSlice from "@/redux/cartSlice";
+
+export const makeStore = configureStore({
+  reducer: {
+    // Products: ProductReducer,
+    Post: postSlice.reducer,
+    Product: productSlice,
+    Cart: cartSlice,
+  },
+});
+
+export type RootState = ReturnType<typeof makeStore.getState>;
+export type AppDispatch = typeof makeStore.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "tailwindcss";
 
+const flowbite = require("flowbite-react/tailwind");
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -15,6 +19,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.content(),
+  ],
 };
 export default config;
